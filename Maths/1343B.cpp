c6f6c742ae1018f545ac814fdd5a009d4c7a35b1
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-04-11 00:16:20 +0530

    1343B.cpp

---
+++ Maths/1343B.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    int n;
+    cin >> n;
+    n /=2;
+    if(n%2){
+        cout<<"NO\n";
+    } else {
+        cout <<"YES\n";
+        f0tn{
+            cout<<(i+1)*2<<" ";
+        }
+        FOR(i,0,n/2){
+            cout<<i*2+1<<" ";
+        }
+        FOR(i,n/2+1,n+1){
+            cout<<i*2+1<<" ";
+        }
+        cout <<"\n";
+    }
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
