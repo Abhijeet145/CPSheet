ec2941ca8db2d31bdff151fe2bbc647289a10064
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-04-21 19:53:45 +0530

    Add 1469B

--- DP/easy/1469B.cpp
+++ DP/easy/1469B.cpp
@@ -23,15 +23,53 @@ typedef vector<vi> vvi;
 
 
 ////////////////////////Template ends////////////////////////
+ll ans;
+//this was the dp solution
+// void util(int i,int j,vi &a,vi &b, ll curr,vvi &dp){
+//     if(i==a.size() && j==b.size()){//if we have moved till end in both just return
+//         ans = max(ans,curr);
+//         return;
+//     } 
+//     ans = max(ans,curr);
+//     if(dp[i][j] != -1) 
+//         return;
+//     //we have two options
+//     //either move one index in a or move 1 index in b
+//     if(i<a.size())
+//         util(i+1,j,a,b,curr+a[i],dp);
+//     if(j<b.size())
+//         util(i,j+1,a,b,curr+b[j],dp);
 
-
+//     dp[i][j] = 1;
+// }
 
 
 void solve(ll tc)
 {
     //start coding here
+    int n,m;
+    cin >> n;
+    int maxa=0,maxb=0;
+    vi a(n);
+    f0tn{
+        cin >> a[i];
+        if(i>0)
+            a[i] += a[i-1];
+        maxa = max(maxa , a[i]);
+    }
+    cin >> m;
+    vi b(m);
+    FOR(i,0,m){
+        cin >> b[i];
+        if(i>0)
+            b[i] += b[i-1];
+        maxb = max(maxb , b[i]);
+    }
+
+    ans = maxa + maxb;
+    //vvi dp(n+1,vi(m+1,-1));
+    //util(0,0,a,b,0,dp);
     
-    ll ans;
     cout <<ans <<"\n";
 }
 
@@ -47,7 +85,7 @@ int main()
     cin.tie(0);
 
     ll t=1,tc=1;
-    //cin >> t;
+    cin >> t;
     while(t--)
     {
         solve(tc++);
