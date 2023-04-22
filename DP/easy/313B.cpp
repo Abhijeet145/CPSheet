c9dbe0501bdf76f359c1f7c1748fe63acf8cec76
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-04-22 11:38:07 +0530

    Add 313B

---
+++ DP/easy/313B.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    string s;
+    cin >> s;
+    int n = s.size();
+    vi a(n+1);
+    FOR(i,1,n){
+        if(s[i]==s[i-1])
+            a[i] = a[i-1] + 1;
+        else
+            a[i] = a[i-1];
+    }
+    int m;
+    cin >> m;
+
+    FOR(i,0,m){
+        int p,q;
+        cin >> p >> q;
+        cout<<a[q-1]-a[p-1]<<"\n";
+    }
+
+
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    //cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
