d15693ce7187327997b6d84cef929066d74385d0
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-04-28 01:57:23 +0530

    Add 1107B.cpp

---
+++ Maths/1107B.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+#define ALL(v) v.begin(),v.end() 
+
+////////////////////////Template ends////////////////////////
+
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll k,x;
+    cin >> k >> x;
+    //this problem requires the prerequisite knowledge of the fact
+    //that the digital sum is 9 if num%9 ==0 and num%9 otherwise
+    ll ans = (k-1)*9 + x;
+    
+	cout <<ans <<"\n";
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
