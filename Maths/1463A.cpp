23a097808d3bae8f7cc4bcd25170a8b6c533afd7
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-05-18 00:14:23 +0530

    Add 1463A

---
+++ Maths/1463A.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+#define ALL(v) v.begin(),v.end() 
+
+////////////////////////Template ends////////////////////////
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll a,b,c;
+    cin >> a >> b >> c;
+    string ans = "NO";
+    ll mini = min(a,min(b,c));
+    ll sum = a + b + c;
+    if((sum %9 == 0) && (mini >= sum/9)){
+        ans = "YES";
+    }
+	cout << ans <<"\n";
+}
+
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
