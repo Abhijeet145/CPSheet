17369ed396c42642976a85ce6eceff716b30c657
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-05-26 00:37:53 +0530

    Add 514A

---
+++ Implementation,Constructive/514A.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll n;
+    cin >> n;
+    string s = "";
+    while(n>0){
+        if(n==9){
+            s = "9"+s;
+            break;
+        }
+        int d = n%10;
+        int take = min(d,9-d);
+        s = to_string(take)+s;
+        n/=10;
+    }
+    ll ans = 0;
+    for(auto x:s){
+        ans = ans*10 + (x-'0');
+    }
+    cout << ans <<"\n";
+
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    //cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
