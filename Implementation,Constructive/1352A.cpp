06c99a6be6bfbd43eaa236c914cb8586b77b27e5
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-05-27 22:57:41 +0530

    Add 1352A.cpp

---
+++ Implementation,Constructive/1352A.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll n;
+    cin >> n;
+    int mul = 0;
+    vi v;
+    while(n>0){
+        int d = n%10;
+        if(d!=0){
+            v.PB(d*pow(10,mul));
+        } 
+        ++mul;
+        n /=10;
+    }
+    cout <<v.size()<<"\n";
+    for(auto x:v){
+        cout << x <<" ";
+    }
+    cout <<"\n";
+
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
