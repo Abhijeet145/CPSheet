c896547694f5e9f042d28f2fed7734751375b70a
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-06-06 11:21:40 +0530

    Add 1303A

---
+++ Implementation,Constructive/1303A.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    string s;
+    cin >> s;
+    int n = s.size();
+    int i=0,j=n-1;
+    while(i<n){
+        if(s[i]=='1')
+            break;
+        ++i;
+    }
+    while(j>=0){
+        if(s[j]=='1')
+            break;
+        --j;
+    }
+    int ans = 0;
+    for(int p=i;p<=j;++p){
+        if(s[p]=='0')
+            ans++;
+    }
+    cout << ans <<"\n";
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
