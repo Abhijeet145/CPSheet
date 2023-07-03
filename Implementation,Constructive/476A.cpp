e8adb7bad54698114f29e29224b31afb8ba68bd7
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-07-04 02:09:04 +0530

    Add 476A

---
+++ Implementation,Constructive/476A.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll n,m;
+    cin >> n >> m;
+
+    ll ans = -1;
+    if(n >= m){
+        ll min_moves = (n+1)/2;
+        if(min_moves%m==0){
+            ans = min_moves;
+        }else{
+            ans = (min_moves/m + 1)*m;
+        }
+    }
+    
+    cout << ans <<"\n";
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    // cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
