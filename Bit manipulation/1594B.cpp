0e5b31fdf8a56239500c8cb694930794944d5f66
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-08-03 14:07:39 +0530

    ADD 1594B

---
+++ Bit manipulation/1594B.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+const ll mod = 1e9+7;
+
+////////////////////////Template ends////////////////////////
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll n,k;
+    cin >> n >> k;
+    ll p = 1;
+    ll ans = 0;
+    
+    FOR(j,0,31){
+        if(k & (1<<j)){
+            ans = (ans + p) % mod;
+        }
+        p *= n;
+        p %= mod;
+    }
+    
+    
+    cout << ans <<"\n";
+
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
